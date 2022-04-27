a0a38507804cc10e3d3d4beaf93fb2c40e8503b2
Chissanu Kittipakorn <chissanuoak2@gmail.com>
2022-04-27 23:33:21 +0700

    Added custom drinks

--- public/pages/js/pump.js
+++ public/pages/js/pump.js
@@ -2,11 +2,18 @@
 var host = window.location.host;
 var socket = io.connect('http://' + host);
 
-var pump1 = document.getElementById('pump1Btn'),
-    pump2 = document.getElementById('pump2Btn'),
-    pump3 = document.getElementById('pump3Btn'),
-    pump4 = document.getElementById('pump4Btn'),
-    pump5 = document.getElementById('pump5Btn');
+var pump1 = document.getElementById('pump1'),
+    pump2 = document.getElementById('pump2'),
+    pump3 = document.getElementById('pump3'),
+    pump4 = document.getElementById('pump4'),
+    pump5 = document.getElementById('pump5'),
+    nextBtn = document.getElementById('nextBtn'),
+    backBtn = document.getElementById('backBtn');
+
+function saveDrink() {
+    document.querySelector('#pump1Text').innerHTML = 'Hide';
+}
+
 
 // Emit events
 pump1.addEventListener('click', function() {
@@ -23,4 +30,15 @@ pump4.addEventListener('click', function() {
 });
 pump5.addEventListener('click', function() {
     socket.emit('pump5Btn');
+});
+nextBtn.addEventListener('click', function() {
+    const drinks = [
+        document.querySelector('#pump1Text').innerHTML,
+        document.querySelector('#pump2Text').innerHTML,
+        document.querySelector('#pump3Text').innerHTML,
+        document.querySelector('#pump4Text').innerHTML,
+        document.querySelector('#pump5Text').innerHTML
+    ];
+
+    console.log(drinks)
 });
