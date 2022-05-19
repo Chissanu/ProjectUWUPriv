fe44d79d0115ab263aa769f1028621d900ae78c0
unknown <chissanuoak2@gmail.com>
2022-05-19 15:07:04 +0700

    Testing array inputs

--- python/pump2.py
+++ python/pump2.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time, sys
 
 inputFromWebApp = 0 #Input from the webapp
-pumpPins = [14,15,16,23,24]
+pumpPins = [14,15,18,23,24]
 inputPin = 25
 glassDetected = False
 
@@ -15,36 +15,42 @@ GPIO.setup(pumpPins[3], GPIO.OUT)
 GPIO.setup(pumpPins[4], GPIO.OUT)
 GPIO.setup(inputPin, GPIO.IN)
 
-# def webapp_to_pump(inputFromWebApp):
-#     if inputFromWebApp == 1:
-#         return pump1
-#     elif inputFromWebApp == 2:
-#         return pump2
-#     elif inputFromWebApp == 3:
-#         return pump3
-#     elif inputFromWebApp == 4:
-#         return pump4
-#     elif inputFromWebApp == 5:
-#         return pump5
-
-webInput = sys.argv[1]
+# webInput = sys.argv[1]
 
 # Take about 11 Seconds to fill water to 300ml
 # The pump pumps water 27.27 ml of water per seconds
 # The maximum number of nums in array can be 27-28
 
-def pumpFromArr(arr, pumpPins):
-    index = 1
-    if glassDetected:
-        for i in arr:
-            for j in range(i):
-                GPIO.output(pumpPins[index - 1], GPIO.LOW) #turn on the pump pouring the liquid
-                time.sleep(1)
-            GPIO.output(pumpPins[index - 1], GPIO.HIGH) #turn off the pump
-            print("Pump", index , "on pin:",pumpPins[index - 1])
-            index += 1
+# def pumpFromArr(arr, pumpPins):
+#     index = 1
+#     if glassDetected:
+#         for i in arr:
+#             for j in range(i):
+#                 GPIO.output(pumpPins[index - 1], GPIO.LOW) #turn on the pump pouring the liquid
+#                 time.sleep(1)
+#             GPIO.output(pumpPins[index - 1], GPIO.HIGH) #turn off the pump
+#             print("Pump", index , "on pin:",pumpPins[index - 1])
+#             index += 1
 
-pumpFromArr(webInput,pumpPins) 
+# pumpFromArr(webInput,pumpPins)
+
+arr = [2,4,2,2,1]
+
+GPIO.output(14, GPIO.HIGH)
+GPIO.output(15, GPIO.HIGH)
+GPIO.output(18, GPIO.HIGH)
+GPIO.output(23, GPIO.HIGH)
+GPIO.output(24, GPIO.HIGH)
+
+for i in range(len(arr)):
+    num = arr[i]
+    for j in range(num):
+        GPIO.output(pumpPins[i], GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(pumpPins[i], GPIO.HIGH)
+    GPIO.output(pumpPins[i], GPIO.HIGH)
+
+
 # while True:
 #     if GPIO.input(inputPin) == 0:
 #         glassDetected = True
